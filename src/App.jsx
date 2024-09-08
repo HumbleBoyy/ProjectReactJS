d649ded1614be32237c9b54b5406b160c743517c
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-08 21:44:32 +0500

    React router dom was added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
+import React from 'react';
 import {
-  Route, 
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import MainLayout from './layouts/MainLayout'
+  RouterProvider,
+  Route
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
-const router = createBrowserRouter(
+
+const routers = createBrowserRouter(
   createRoutesFromElements(
-   <Route path='/' element={<MainLayout/>}>
-     <Route index element={<HomePage/>}/>
-  </Route>)
-)
+     <Route path='/' element={<MainLayout/>}>
+           <Route index element={<HomePage/>}/>
+           <Route path='/jobs' element={<JobsPage/>}/>
+     </Route>
+  )
+);
 
 const App = () => {
-  return <RouterProvider />
-}
+  return <RouterProvider router={routers} />;
+};
 
-export default App
+export default App;
+
