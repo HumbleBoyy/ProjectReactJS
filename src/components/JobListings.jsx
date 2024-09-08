c21b3796c33e5da31b097a5cf4d653b41e19a700
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-09-08 22:12:19 +0500

    Navbar menu was made interactive

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
 import Spinner from './Spinner';
 import jobs from "../jobs.json"
-const JobListings = () => {
+const JobListings = ({ isHome = false }) => {
 
-  const recentJobs = jobs.jobs.slice(0, 3)
+  const jobListings = isHome ? jobs.jobs.slice(0, 3) : jobs;
   
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+        </h2>
       <div class="grid grid-cols-2 md:grid-cols-3 gap-6">
-              {recentJobs.map((job)=> (
+              {jobListings.map((job)=> (
                 <>
                   <JobListing key={job.id} job={job}/>
                </>
